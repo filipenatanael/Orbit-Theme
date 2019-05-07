e0ac797d6e01b4b4bc5d5ff4c4a7fbceaae9e764
Filipe Natanael <filipenatanael@live.com>
2019-05-06 22:37:40 -0300

    refactor: added projects-container to component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Languages from './components/Languages';
 import Interests from './components/Interests';
 import Summary from './components/Summary';
 import Experiences from './components/Experiences';
+import Projects from './components/Projects';
 
 class App extends Component {
   render() {
@@ -32,29 +33,8 @@ class App extends Component {
            {/*<!--experiences-section-->*/}
            <Experiences />
 
-             <section className="section projects-section">
-                 <h2 className="section-title"><span className="icon-holder"><i className="fas fa-archive"></i></span>Projects</h2>
-                 <div className="intro">
-                     <p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.</p>
-                 </div>{/*<!--//intro-->*/}
-                 <div className="item">
-                     <span className="project-title"><a href="#hook">Velocity</a></span> - <span className="project-tagline">A responsive website template designed to help startups promote, market and sell their products.</span>
-
-                 </div>{/*<!--//item-->*/}
-                 <div className="item">
-                     <span className="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-web-development-agencies-devstudio/" target="_blank">DevStudio</a></span> -
-                     <span className="project-tagline">A responsive website template designed to help web developers/designers market their services. </span>
-                 </div>{/*<!--//item-->*/}
-                 <div className="item">
-                     <span className="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-startups-tempo/" target="_blank">Tempo</a></span> - <span className="project-tagline">A responsive website template designed to help startups promote their products or services and to attract users &amp; investors</span>
-                 </div>{/*<!--//item-->*/}
-                 <div className="item">
-                     <span className="project-title"><a href="hhttp://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-mobile-apps-atom/" target="_blank">Atom</a></span> - <span className="project-tagline">A comprehensive website template solution for startups/developers to market their mobile apps. </span>
-                 </div>{/*<!--//item-->*/}
-                 <div className="item">
-                     <span className="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-mobile-apps-delta/" target="_blank">Delta</a></span> - <span className="project-tagline">A responsive Bootstrap one page theme designed to help app developers promote their mobile apps</span>
-                 </div>{/*<!--//item-->*/}
-             </section>{/*<!--//section-->*/}
+           {/*<!--projects-section-->*/}
+           <Projects />
 
              <section className="skills-section section">
                  <h2 className="section-title"><span className="icon-holder"><i className="fas fa-rocket"></i></span>Skills &amp; Proficiency</h2>

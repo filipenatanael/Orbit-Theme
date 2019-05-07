1688d71b7119d9b680d47b111b2fb29c14b4d1f0
Filipe Natanael <filipenatanael@live.com>
2019-05-06 21:38:58 -0300

    refactor: added interests container to component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Profile from './components/Profile';
 import Contact from './components/Contact';
 import Education from './components/Education';
 import Languages from './components/Languages';
+import Interests from './components/Interests';
 
 class App extends Component {
   render() {
@@ -21,17 +22,12 @@ class App extends Component {
             {/*<!--languages-container-->*/}
             <Languages />
 
-             {/*<!--interests-->*/}
-             <div className="interests-container container-block">
-                 <h2 className="container-block-title">Interests</h2>
-                 <ul className="list-unstyled interests-list">
-                     <li>Climbing</li>
-                     <li>Snowboarding</li>
-                     <li>Cooking</li>
-                 </ul>
-             </div>{/*<!--//interests-->*/}
+            {/*<!--interests-container-->*/}
+            <Interests />
 
-         </div>{/*<!--//sidebar-wrapper-->*/}
+         {/*<!--sidebar-wrapper-->*/}
+         </div>
+
 
          <div className="main-wrapper">
 
---
+++ src/components/Interests.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Interests = ({ props }) => (
+  <div className="interests-container container-block">
+      <h2 className="container-block-title">Interests</h2>
+      <ul className="list-unstyled interests-list">
+          <li>Climbing</li>
+          <li>Snowboarding</li>
+          <li>Cooking</li>
+      </ul>
+  </div>
+);
+
+export default Interests;

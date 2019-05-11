2a1694f13838b7d6072db1ae1fc0d9487882be62
Filipe Natanael <filipenatanael@live.com>
2019-05-10 23:51:10 -0300

    chore: set up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './components/Main';
 
 class App extends Component {
   render() {
     return (
-      <Main />
+      <BrowserRouter>
+        <Fragment>
+          <Route path="/" exact component={Main} />
+        </Fragment>
+      </BrowserRouter>
    );
   }
 }

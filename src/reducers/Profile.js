9941ac3281d95ac5b6248decc23c38c75da60954
Filipe Natanael <filipenatanael@live.com>
2019-05-12 21:33:39 -0300

    feat: map state to props on summary

--- src/reducers/Profile.js
+++ src/reducers/Profile.js
@@ -3,7 +3,16 @@ const initialState = {
     photo: '',
     name: 'Filipe Natanael',
     title: 'Full Stack Developer',
-    email: 'filipenatanael@example.com'
+    email: 'filipenatanael@example.com',
+    phone: '00 00000-00000',
+    portifolio: '@filipenatanael',
+    linkedin: '@filipenatanael',
+    github: '@filipenatanael',
+    twitter: '@filipenatanael'
+  },
+  summary: {
+    title: 'CAREER PROFILE',
+    content: 'Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can download this free resume/CV template here. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.'
   }
 }
 

f2e0c4959df26546023ea7811a42b7137bf7ae60
Filipe Natanael <filipenatanael@live.com>
2019-05-27 15:20:38 +0100

    feat: adjusting flag to change language

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,28 @@ import { connect } from 'react-redux';
 import { fetchProfile } from '../actions';
 
 const Profile = ( props ) => (
-  <div className="profile-container">
+  <div className="profile-container" style={{ backgroundColor: '#000' }}>
       <img className="profile" src="https://themes.3rdwavemedia.com/demo/orbit/assets/images/profile.png" alt="" />
       <h1 className="name" style={{ fontSize: 25 }}>{props.general.name}</h1>
       <h3 className="tagline">{props.general.title}</h3>
-      <button type="button" onClick={() => props.fetchProfile()}>Click Me!</button>
+      <div style={{
+        padding: 4,
+        boxShadow: '7px 7px 5px #9E9E9E',
+        position: 'absolute',
+        top: 0,
+        right: 0
+        }}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/4/45/Flag_of_Ireland.svg"
+            alt="select language"
+            height="25"
+            width="42"
+            />
+      </div>
   </div>
 );
+
+// <button type="button" onClick={() => props.fetchProfile()}>Click Me!</button>
 
 const mapStateToProps = state => ({
   general: state.profile.general

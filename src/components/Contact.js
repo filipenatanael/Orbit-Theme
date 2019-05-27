121faf2e0e233d4aa5ac90a772a8c10cd7f28809
Filipe Natanael <filipenatanael@live.com>
2019-05-27 22:10:13 +0100

    chore: added lodash

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-const Contact = ({ props }) => (
+const Icons = {
+  'email': 'fas fa-envelope',
+  'phone': 'fas fa-phone',
+  'website': 'fas fa-globe',
+  'linkedin': 'fab fa-linkedin-in',
+  'github': 'fab fa-github',
+  'twitter': 'fab fa-twitter'
+};
+
+const Contact = ( props ) => (
   <div className="contact-container container-block">
       <ul className="list-unstyled contact-list">
-          <li className="email"><i className="fas fa-envelope"></i><a href="mailto: yourname@email.com">alan.doe@website.com</a></li>
-          <li className="phone"><i className="fas fa-phone"></i><a href="tel:0123 456 789">0123 456 789</a></li>
-          <li className="website"><i className="fas fa-globe"></i><a href="https://themes.3rdwavemedia.com/bootstrap-templates/resume/orbit-free-resume-cv-bootstrap-theme-for-developers/" target="_blank">portfoliosite.com</a></li>
-          <li className="linkedin"><i className="fab fa-linkedin-in"></i><a href="#" target="_blank">linkedin.com/in/alandoe</a></li>
-          <li className="github"><i className="fab fa-github"></i><a href="#" target="_blank">github.com/username</a></li>
-          <li className="twitter"><i className="fab fa-twitter"></i><a href="https://twitter.com/3rdwave_themes" target="_blank">@twittername</a></li>
+        {
+          _.map(props.contact, c => {
+            let icon = Icons[c.type];
+            return (
+              <li className={c.type}><i className={icon}></i>
+               <a href={c.url} target={c.target === true ? '_blank' : ''}>{c.content}</a></li>
+              );
+          })
+        }
       </ul>
   </div>
 );
 
-export default Contact;
+const mapStateToProps = (state) => ({
+  contact: state.profile.general.contact
+});
+
+export default connect(mapStateToProps)(Contact);

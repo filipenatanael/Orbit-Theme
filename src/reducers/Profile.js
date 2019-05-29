d6f1d2d34161fea5f83333ad37667f619c1c5331
Filipe Natanael <filipenatanael@live.com>
2019-05-29 21:51:30 +0100

    feat: implemented experience reducer structure

--- src/reducers/Profile.js
+++ src/reducers/Profile.js
@@ -53,6 +53,29 @@ const initialState = {
     title: 'CAREER PROFILE',
     content: 'I am fascinated for software architecture and design, I have over 7 years of experience working with technology, of these, 5 years was in software development using patterns, newest technologies, and frameworks such as Laravel, React and Vue. I have a strong knowledge building complex SPA applications/services and mobile applications using React-Native. I have familiarity with the progressive web apps concepts as well as I can apply it for building rich web application. I am always trying to learn more and open to new difficult tech challenges.'
   },
+  experiences: {
+    title: 'Experiences',
+    jobs: [
+      {
+        title: 'Lead Developer',
+        time: '2015 - Present',
+        company: 'Startup Hubs, San Francisco',
+        details: '<p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>'
+      },
+      {
+        title: 'Senior Software Engineer',
+        time: '2014 - 2015',
+        company: 'Google, London',
+        details: '<p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>'
+      },
+      {
+        title: 'UI Developer',
+        time: '2012 - 2014',
+        company: 'Amazon, London',
+        details: '<p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>'
+      }
+    ]
+  },
 
   github: {}
 }

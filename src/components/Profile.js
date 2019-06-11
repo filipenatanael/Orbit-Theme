5e54f8ab39f0f1e49e63aaac1db630f1424b0089
Filipe Natanael <filipenatanael@live.com>
2019-06-11 11:47:37 +0100

    refactor: desable flags icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,26 +7,25 @@ const Profile = ( props ) => (
       <img className="profile" src={props.general.photo} alt="Profile" />
       <h1 className="name" style={{ fontSize: 25 }}>{props.general.name}</h1>
       <h3 className="tagline">{props.general.title}</h3>
-      <div style={{
-        // padding: 4,
-        // boxShadow: '7px 7px 5px #9E9E9E',
-        position: 'absolute',
-        marginRight: -50,
-        top: 0,
-        right: 0
-        }}>
-          <img
-            src={props.config.flag}
-            alt={props.config.alternate}
-            height="25"
-            width="42"
-            onClick={() => props.fetchProfile()}
-            />
-      </div>
   </div>
 );
-
-// <button type="button" onClick={() => props.fetchProfile()}>Click Me!</button>
+/* <div style={{
+    // padding: 4,
+    // boxShadow: '7px 7px 5px #9E9E9E',
+    position: 'absolute',
+    marginRight: -50,
+    top: 0,
+    right: 0
+    }}>
+      <img
+        src={props.config.flag}
+        alt={props.config.alternate}
+        height="25"
+        width="42"
+        />
+  </div> */
+  
+// onClick={() => props.fetchProfile('french')}
 
 const mapStateToProps = state => ({
   config: state.app.config,
